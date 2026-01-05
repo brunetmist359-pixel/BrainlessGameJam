be02641015207b27d6b9bec819df91bb299b3ac1
brunetmist359-pixel <brunetmist359@gmail.com>
2026-01-05 00:47:03 -0600

    Vegetable movement AI

--- BrainlessGameJamUnity/Assets/Scripts/Veggie1.cs
+++ BrainlessGameJamUnity/Assets/Scripts/Veggie1.cs
@@ -7,21 +7,39 @@ public class Veggie1 : MonoBehaviour
     // Defining Variables
     public float speed;
     private Rigidbody2D rb;
+
+    public List<Transform> waypoints;
     
-    
+
+    // pathfinding variables
+    private int CurrentMovePoint = 0;
+
     // assigning object components to variable
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.WakeUp();
-        Debug.Log("start has ran");
+       
     }
 
     // Move from right to left
     void FixedUpdate()
     {
-        rb.velocity = Vector2.left * speed;
-        Debug.Log("Update is running");
+        if (waypoints == null || waypoints.Count == 0) return;
+
+        Transform target = waypoints[CurrentMovePoint];
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        
+        Vector3 direction = (target.position - transform.position).normalized;
+       
+
+        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        {
+            CurrentMovePoint++;
+            if (CurrentMovePoint >= waypoints.Count)
+            {
+                DestroySelf();
+            }
+        }
     }
 
     // collision checks 
--- BrainlessGameJamUnity/Assets/Scripts/WaveManager.cs
+++ BrainlessGameJamUnity/Assets/Scripts/WaveManager.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class WaveManager : MonoBehaviour
 {
     public GameObject Veggie1prefab;
+
+    // Defining possible spawn locations
     public GameObject SpawnPoint1;
     public GameObject SpawnPoint2;
     public GameObject SpawnPoint3;
 
+    //create List for pathfinding
+    public List<Transform> TopPathNodes;
+    public List<Transform> MiddlePathNodes;
+    public List<Transform> BottomPathNodes;
+
     //create list for spawnpoints to live in
     public List<GameObject> SpawnPoints = new List<GameObject>();
 
+    
+ 
     // Assign SpawnPoints to list
     void Awake()
     {
@@ -23,7 +32,7 @@ public class WaveManager : MonoBehaviour
     // Will eventually be in charge of calling and updating wavestate
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
             Spawn(Veggie1prefab);
         }
@@ -32,9 +41,16 @@ public class WaveManager : MonoBehaviour
     // Spawns a veggie at a random spawn point every time its called
     public void Spawn(GameObject enemytype)
     {
+        List<Transform> SelectedPath = TopPathNodes;
+
         int randomindex = Random.Range(0, SpawnPoints.Count);
         GameObject spawnpoint = SpawnPoints[randomindex];
 
-        Instantiate(enemytype, spawnpoint.transform.position, Quaternion.identity);
+        if (randomindex == 0) SelectedPath = TopPathNodes;
+        else if (randomindex == 1) SelectedPath = MiddlePathNodes;
+        else if (randomindex == 2) SelectedPath = BottomPathNodes;
+
+        GameObject enemy = Instantiate(enemytype, spawnpoint.transform.position, Quaternion.identity);
+        enemy.GetComponent<Veggie1>().waypoints = SelectedPath;
     }
 }
