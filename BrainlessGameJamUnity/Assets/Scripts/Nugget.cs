3ffb2a3b0aa3055a78a68bde92d415c08cc15f51
Allison Bianchi <2694508+agb63@users.noreply.github.com>
2026-01-05 12:06:15 -0800

    Tweak nugget behavior and move launcher to bottom-left

--- BrainlessGameJamUnity/Assets/Scripts/Nugget.cs
+++ BrainlessGameJamUnity/Assets/Scripts/Nugget.cs
@@ -6,17 +6,20 @@ public class Nugget : MonoBehaviour
 {
     private Rigidbody2D rb;
 
+    public Vector2 startPosition;
     public float launchForce;
     public Vector2 launchDir;
 
     void Awake()
     {
-        rb= GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        //GetComponent<Transform>().position = new Vector3(startPosition[0], startPosition[1], 0);
+        //rb.position = startPosition;
     }
 
     // Update is called once per frame
@@ -24,27 +27,31 @@ public class Nugget : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Reset();
+            ResetForce();
             rb.AddForce(launchDir * launchForce, ForceMode2D.Impulse);
         }
         else if (Input.GetKeyDown(KeyCode.R))
         {
-            Reset();
+            ResetForce();
+            ResetPosition();
         }
     }
-    
-    private void Reset()
+
+    private void ResetForce()
     {
         // Clear linear and angular velocity
         rb.velocity = Vector2.zero;
         rb.angularVelocity = 0;
 
-        // Reset Z-rotation and position
-        rb.rotation = 0;
-        rb.position = new Vector2(0, 0);
-        
         // Optional: Put the Rigidbody to sleep to prevent minor forces (like gravity over a single frame) 
         // from immediately affecting it again, ensuring it stays still until a new force is applied.
-        rb.Sleep();
+        //rb.Sleep();
+    }
+
+    private void ResetPosition()
+    {
+        // Reset Z-rotation and position
+        rb.rotation = 0;
+        rb.position = startPosition;
     }
 }
